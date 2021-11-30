aff428f10b988710b5f20a5d8f5d36657c519981
Andy Alexander <andrew.j.alexander@gmail.com>
2021-11-29 20:38:08 -0500

    Update per meeting

--- src/components/modals/ClassModal.js
+++ src/components/modals/ClassModal.js
@@ -1,7 +1,43 @@
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Component } from "react";
 import { LightBlueText } from "../../components/StyledText";
+import GoogleMapReact from "google-map-react";
+import Geocode from "react-geocode";
+
+class SimpleMap extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latlng: this.generateLatLng(),
+    };
+    this.generateLatLng = this.generateLatLng.bind(this);
+  }
+
+  async generateLatLng() {
+    if (this.props.location !== null) {
+      const latlng = await Geocode.fromAddress(this.props.location);
+      this.setState({ latlng });
+    } else {
+      return null;
+    }
+  }
+
+  render() {
+    if (this.state.latlng === null) {
+      return <div />;
+    }
+    return (
+      <div style={{ height: 200, width: 200 }}>
+        <GoogleMapReact
+          //bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
+          defaultCenter={this.state.latlng}
+          defaultZoom={14}
+        ></GoogleMapReact>
+      </div>
+    );
+  }
+}
 
 export default function ClassModal({ ...props }) {
   const handleClose = () => props.setValue(null);
@@ -24,6 +60,9 @@ export default function ClassModal({ ...props }) {
         <div
           dangerouslySetInnerHTML={{ __html: props.classValue.details }}
         ></div>
+        <div style={{ position: "absolute", top: 150, right: 20 }}>
+          <SimpleMap location={props.classValue.location} />
+        </div>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={handleClose}>
--- src/controllers/classController.js
+++ src/controllers/classController.js
@@ -8,11 +8,18 @@ export async function getClasses() {
       date_gte: new Date().toLocaleDateString("en-CA"),
     },
   });
+
+  // TODO move to more general controller?
+  const formatDate = (date) => {
+    const dateObj = new Date(date);
+    return dateObj.toLocaleDateString("en-US");
+  };
+
   const classes = response.data.map((item) => {
     return {
       id: item.id,
-      date: item.date,
-      money_date: item.money_date,
+      date: formatDate(item.date),
+      money_date: formatDate(item.money_date),
       city: item.city.city,
       class_type: item.class_type.class_type,
       state: item.state.state,
@@ -21,6 +28,7 @@ export async function getClasses() {
       information: item.information,
       other_information: item.other_information,
       classroom_location: item.classroom_location,
+      location: item.location,
       title: item.title,
       // change to reflect braintree standards?
       amount: item.cost,
